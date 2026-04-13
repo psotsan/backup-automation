fad98b2d1986a150643fbf1c84271eef259a45b6
Pedro <psotsan@pm.me>
2026-04-13 20:12:52 +0200

    First commit. Beginning of backup.sh. Basic configuration file. README.md

---
+++ README.md
@@ -0,0 +1,22 @@
+ # Robust Backup Automation
+                                            
+ A modular Bash automation suite for reliable backups, validation, and logging. Active Development.
+
+ ## Development Roadmap
+                                               
+ | Phase | Status |
+ | :--- | :--- |                                                   
+ | 1. Config & Init | WIP |
+ | 2. Logging Module | Pending |
+ | 3. Logging | Pending |                                      
+ | 4. Core Logic | Pending |
+ | 5. Docs & Tests | Pending |
+                                          
+## Current Capabilities
+WIP                                                           
+  
+## Tech Stack
+                                                           
+- Language: Bash 5.2
+- Dependencies: coreutils
+- Editor: Vim
---
+++ backup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
+process_args() {
+    echo "$#"
+    if [[ $# -gt 0 && $(($# % 2)) -eq 0 ]]; then
+        while [[ "$#" > 0 ]]; do
+            case "$1" in
+                -c) shift
+                    CONFIG_FILE="$1"
+                    shift
+                    ;;
+                # Use ERROR when logging module is implemente
+                *) echo "[-] Unknown option $1" >&2
+                   exit 1
+                   ;;
+            esac
+        done
+    else
+        # Use ERROR when logging module is omplemented
+        echo "[-] Incorrect number of options" >&2
+        exit 1
+    fi
+}
+
+
+read_config() {
+    while IFS='=' read -r key val; do
+        [[ "$key" =~ ^#.*$ ]] && continue
+        [[ -z "$key" ]] && continue
+
+        key=$(echo "$key" | xargs)
+        val=$(echo "$val" | xargs)
+
+        case "$key" in
+            USER) USER="$val";;
+            SECRET) SECRET="$val";;
+            SOURCE_DIR) SOURCE_DIR="$val";;
+            DEST_DIR) DEST_DIR="$val";;
+            # Use WARNING when logging  module is implemented
+            *) echo "[-] Unknown variable $key ignored" >&2;;
+        esac
+    done < "$CONFIG_FILE"
+}
+
+
+process_args $@
+echo "[+] CONFIG_FILE = $CONFIG_FILE"
+
+if [[ -z "$CONFIG_FILE" ]]; then
+    CONFIG_FILE="config/config.conf"
+fi
+
+echo "[+] CONFIG_FILE = $CONFIG_FILE"
+read_config "$CONFIG_FILE"
