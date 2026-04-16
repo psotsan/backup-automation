f5ffcfb620b70012a1af464115298d1d0c5fd732
Pedro <psotsan@pm.me>
2026-04-16 19:08:23 +0200

    Validation module done

--- backup.sh
+++ backup.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
 . ./lib/logging.sh
+. ./lib/validation.sh
 
 set -euo pipefail
 
 
 process_args() {
+    local action="PROCESSING_ARGS"
     echo "$#"
     if [[ $# -gt 0 && $(($# % 2)) -eq 0 ]]; then
-        while [[ "$#" > 0 ]]; do
+        while [[ "$#" -gt 0 ]]; do
             case "$1" in
                 -c) shift
                     CONFIG_FILE="$1"
                     shift
                     ;;
-                # Use ERROR when logging module is implemente
                 *) echo "[-] Unknown option $1" >&2
+                    log_error "$action" "Unknow option $1"
                    exit 1
                    ;;
             esac
         done
     else
-        # Use ERROR when logging module is omplemented
         echo "[-] Usage: $0 -c <config_file>" >&2
+        log_error "$action" "Script misuse"
         exit 1
     fi
 }
 
 
 read_config() {
+    local action="READ_CONFIG"
     while IFS='=' read -r key val; do
         [[ "$key" =~ ^#.*$ ]] && continue
         [[ -z "$key" ]] && continue
@@ -41,16 +44,23 @@ read_config() {
             SECRET) SECRET="$val";;
             SOURCE_DIR) SOURCE_DIR="$val";;
             DEST_DIR) DEST_DIR="$val";;
+            DST_FREE_THR) DST_FREE_THR="$val";;
             # Use WARNING when logging  module is implemented
-            *) echo "[-] Unknown variable $key ignored" >&2;;
+            *) echo "[-] Unknown variable $key ignored" >&2
+            log_warning "$action" "Unkown variable defined in CONFIG_FILE"
+            ;;
         esac
     done < "$CONFIG_FILE"
 }
 
 
+run_pre_checks() {
+    
+}
+
 log_debug "START" "Testing logging module"
 
-process_args $@
+process_args "$@"
 echo "[+] CONFIG_FILE = $CONFIG_FILE"
 
 if [[ -z "$CONFIG_FILE" ]]; then
--- lib/validation.sh
+++ lib/validation.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# TODO validate_dst_free function
-
+# shellcheck source=logging.sh
 . ./logging.sh
+
 
 validate_root() {
     local action="VALIDATE_ROOT_USER"
-    if [[ $UID -eq 0 ]]; then
-        log_info "$action" "User IS root"
-        return 0
-    else
+    if [[ $UID -ne 0 ]]; then
         log_error "$action" "User IS NOT root"
         return 1
-    fi
+    fi    
+    
+    log_info "$action" "User IS root"
 }
 
 
@@ -20,13 +19,13 @@ validate_conf_file() {
     local action="VALIDATE_CONFIG_FILE"
     local conf_file="$1"
 
-    if [[ -r conf_file ]]; then
-        log_info "$action" "File $conf_file IS readable"
-        . "$conf_file"
-    else
-        log_error "$action" "File $conf_file IS NOT readable"
+    if [[ ! -r conf_file ]]; then
+        log_info "$action" "File $conf_file IS NOT readable"
         return 1
     fi
+    
+    log_error "$action" "File $conf_file IS readable"
+    . "$conf_file"
 }
 
 
@@ -34,20 +33,19 @@ validate_src_dir() {
     local dir="$1"
     local action="VALIDATE_SOURCE_DIR"
     
-    if [[ -d "$dir" ]]; then
-        log_info "$action" "Source directory $dir exists"
-    else
-        log_error "$action" "Source directory $dir DOES NOT exist"
+    if [[ ! -d "$dir" ]]; then
+        log_info "$action" "Source directory $dir DOES NOT exist"
         return 1
     fi
 
-    if [[ -r "$dir" ]]; then
-        log_info "$action" "Source directory $dir IS readable"
-        return 0
-    else
-        log_error "$action" "Source directory $dir IS NOT readable"
+    log_error "$action" "Source directory $dir exists"
+
+    if [[ ! -r "$dir" ]]; then
+        log_info "$action" "Source directory $dir IS NOT readable"
         return 1
     fi
+
+    log_error "$action" "Source directory $dir IS readable"
 }
 
 
@@ -55,21 +53,36 @@ validate_dst_dir() {
     local dir="$1"
     local action="VALIDATE_DESTINATION_DIR"
     
-    if [[ -d "$dir" ]]; then
-        log_info "$action" "Destination directory $dir exists"
-    else
-        log_error "$action" "Destinantion directory $dir DOES NOT exist"
+    if [[ ! -d "$dir" ]]; then
+        log_info "$action" "Destination directory $dir DOES NOT exists"
         return 1
     fi
 
-    if [[ -w "$dir" ]]; then
-        log_info "$action" "Destination directory $dir IS writable"
-    else
-        log_error "$action" "Destination directory $dir IS NOT writable"
+    log_error "$action" "Destinantion directory $dir exists"
+
+    if [[ ! -w "$dir" ]]; then
+        log_info "$action" "Destination directory $dir IS NOT writable"
         return 1
     fi
+    
+    log_error "$action" "Destination directory $dir IS writable"
+}
 
 
+validate_dst_free() {
+    local dir=$1
+    local threshold=$2
+    local action="VALIDATE_FREE_SPACE_ON_DESTINATION"
+    local free
+    free=$(df "$dir" | tail -n 1 | awk '{print $5}' | cut -d % -f 1)
+
+
+    if [[ $free -lt $threshold ]]; then
+        log_error "$action" "Not enough free space in $dir"
+        exit 1
+    fi
+
+    log_info "$action" "Enough free space in $dir"
 }
 
 
